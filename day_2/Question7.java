0905540830abdac649fda64cdb5252e76070ce5a
Azat <81050987+azatbayram@users.noreply.github.com>
2021-03-21 01:25:50 +0300

    Initial commit

---
+++ day_2/Question7.java
@@ -0,0 +1,12 @@
+package Assignments.day_2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" /\\  /\\");
+		System.out.println("/__\\/__\\");
+
+	}
+
+}
---
+++ day_3/assignment2_variables.java
@@ -0,0 +1,82 @@
+package Assignments.day_3;
+
+public class assignment2_variables {
+
+	public static void main(String[] args) {
+		
+		// Question-1
+		int count=26;
+		
+		System.out.println("Q1\n" + count);
+		System.out.println("\n--------------\n");
+		
+		//Question-2
+		String message="I will call you later.";
+		
+		System.out.println("Q2\n" + message);
+		System.out.println("\n--------------\n");
+		
+		//Question-3
+		int num=15;
+		double cost=36.54;
+		
+		System.out.println("Q3\n" + num +" "+ cost);
+		System.out.println("\n--------------\n");
+		
+		//Question-4
+		String ans="Java";
+		
+		System.out.println("Q4\nToday's Word-of-the-day is: " + ans);
+		System.out.println("\n--------------\n");
+		
+		//Question-5
+		byte first=55, second=123;
+		
+		System.out.println("Q5\nfirst is " + first + "second = " + second);
+		System.out.println("\n--------------\n");
+		
+		//Question-6
+		String seat1, seat2, seat3, seat4, seat5;
+		seat1="Bob";
+		seat2="Jill";
+		seat3="Betty";
+		seat4="Herb";
+		seat5="James";
+		
+		System.out.println("Q6\n"+seat1+", "+seat2+", "+seat3+", "+seat4+", "+seat5);
+		System.out.println("\n--------------\n");
+		
+		//Question-7
+		int i=9;
+		float f=33.4F;
+		
+		System.out.println("i="+i+" f="+f);
+		System.out.println("\n--------------\n");
+		
+		//Question-8
+		String lan1="\"Java\"";
+		String lan2="\"SQL\"";
+		
+		System.out.println("I will learn "+lan1+" and "+lan2+" at CybertekSchool.");
+		System.out.println("\n--------------\n");
+		
+		//Question-9
+		byte steps=100;
+		short miles=5000;
+		int count1=1000000;  //I declare it as "count1" becouse I already used same name in Question-1
+		long population=3434455667L;
+		
+		//Question-10
+		int hour=12, minute=24, second1=33; //I declare it as "second1" becouse I already used same name in Question-5
+		String amOrPm="PM";
+		
+		System.out.println(hour+":"+minute+":"+second1+" "+amOrPm);
+		
+		
+	
+		
+		
+
+	}
+
+}
